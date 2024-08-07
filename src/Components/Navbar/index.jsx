c706336d73bc60d2ea9fec1ef509dd7f59e5f876
camilo <camiloramos99@outlook.com.ar>
2024-08-06 22:48:51 -0300

    feat: create Navbar component with NavLink for user navigation indication

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,115 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const activeStyle = "underline underline-offset-4"
+
+  
+
+
+    return (
+        <nav className="flex justify-between items-center fixed w-full z-10 py-5 px-8 text-sm font-normal">
+            <ul className="flex items-center gap-3" >
+                <li className="font-semibold text-lg">
+                    <NavLink 
+                        to="/">
+                        BestBuyMarket
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }> 
+                        All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/clothes"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>                     
+                        Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/electronics"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        Electronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/furnitures"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        Furnitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/toys"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/others"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        Others
+                    </NavLink>
+                </li>
+            </ul>    
+            <ul className="flex justify-between gap-3">
+                <li className="text-black/60">
+                    BestBuyMarket@gmail.com
+                </li>
+                <li>
+                    <NavLink 
+                        to="/my-orders"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/my-account"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/sign-in"
+                        className={({ isActive }) =>    
+                            isActive ? activeStyle : undefined      
+                        }>  
+                        Sign In
+                    </NavLink>
+                </li>
+                <li>
+                    🛒0
+                </li>
+            
+            </ul>   
+        </nav>
+        
+    )
+}
+
+
+export default Navbar
