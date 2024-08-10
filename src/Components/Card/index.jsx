393cc2a96607706044516452f84ec718c95ecb97
camilo <camiloramos99@outlook.com.ar>
2024-08-09 23:57:58 -0300

    Consuming API to fetch products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,8 +1,8 @@
-const Card = () => {
+const Card = (data) => {
     return (
         <article className="bg-white cursor-pointer h-60 w-56 rounded-lg">
             <figure className="relative mb-2 w-full h-4/5">
-            <caption className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</caption>
+            <caption className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name}</caption>
             <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Headphones" />
 
             <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"> + </button>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+import { useEffect } from "react"
 
 function Home() {
+
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
+
+
     return (
       <Layout>
         <div className='text-center font-bold	' >
             Home
-            <Card />
+            {
+              items?.map(item => (
+                <Card key={item.id} data={item} />
+              ))
+            }
         </div>
       </Layout>
 
