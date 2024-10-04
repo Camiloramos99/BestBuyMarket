30f4a93cc27285c8f7b06eed05f61e8f2143b132
camilo <camiloramos99@outlook.com.ar>
2024-10-04 02:17:48 -0300

    feat: view my account design

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -3,9 +3,51 @@ import Layout from "../../Components/Layout"
 function MyAccount() {
     return (
       <Layout>
-        <div className='bg-cyan-400' >
-            MyAccount
-        </div>  
+        <div className="flex items-center justify-center relative w-full h-[calc(100vh-80px)]">
+            <div className="login-container flex flex-col h-full w-80 ">
+                <title className="flex flex-col font-sans font-semibold text-xl mb-6 ">My account</title>
+ 
+                <form action="/" 
+                    className="form flex flex-col mt-1 h-full w-full" >
+                <label 
+                    htmlFor="Name"
+                    className="font-medium font-sans mb-1"
+                    >Name
+                </label>
+                <input 
+                    type="text" 
+                    id="name"
+                    placeholder="your name"
+                    className="input input-email bg-text-input-field text-base rounded-lg h-10 p-2 mb-5 "
+                    />
+                <label 
+                    htmlFor="email" 
+                    className="font-medium font-sans mb-1"
+                    >Email
+                </label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    placeholder="camiloramos99@outlook.com" 
+                    className="input input-email bg-text-input-field text-base rounded-lg h-10 p-2 mb-5 " />
+                
+                <label 
+                    htmlFor="password" 
+                    className="font-medium font-sans mb-1"
+                    >Password
+                </label>
+                <input 
+                    type="password" 
+                    id="password"   
+                    placeholder="***********" 
+                    className=" bg-text-input-field text-base rounded-lg h-10 p-2 mb-5 "/>
+    
+                <button className="place-self-center font-bold bg-purple-500 text-white rounded-lg h-12 w-full mt-6 mb-7">Log out</button>
+                
+                </form>
+
+            </div>     
+        </div>   
       </Layout>
     )
   }
