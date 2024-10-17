0b09147dec49fe199f5410700b45626334d1ee29
camilo <camiloramos99@outlook.com.ar>
2024-10-17 02:28:00 -0300

    refactor(Navbar): Extract ShoppingCart component and import in Navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { ShopingCartContext } from "../../Context";
 import { NavLink } from "react-router-dom";
+import ShoppingCart from '/src/Components/ShoppingCart';
+
 
 const Navbar = () => {
     const activeStyle = "underline underline-offset-4"
@@ -136,23 +138,7 @@ const Navbar = () => {
             </ul>    
             <ul className="flex justify-between gap-3">
                 {renderView()}
-                <li className="relative">
-                    <svg 
-                        className="absolute size-6" 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" viewBox="0 0 24 24" 
-                        strokeWidth="1.5" 
-                        stroke="currentColor" > 
-                        <path strokeLinecap="round" 
-                        strokeLinejoin="round" 
-                        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" 
-                        />
-                    </svg>
-                    <span 
-                        className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 py-1 text-xs transform translate-x-1/2 -translate-y-1/2"
-                        >{ CartProducts.length }
-                    </span>
-                </li>
+                <ShoppingCart />
             </ul>   
         </nav>
         
