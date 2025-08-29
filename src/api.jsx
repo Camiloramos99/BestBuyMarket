8600739ee787ea814df795edd9e58a4b105c8831
camilo <camiloramos99@outlook.com.ar>
2025-08-29 13:28:08 -0300

    feat(products): load local JSON until API response arrives

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,29 @@
 // URL base de la API
 const API_BASE_URL = "https://teststoreapi.onrender.com";
 
+import localProducts from "../src/assets/products.json";
+
 // Función para obtener productos de la API
 export const fetchProducts = async () => {
-  const response = await fetch(`${API_BASE_URL}/productos`);
 
-  const data = await response.json();
-  return data;
+  try {
+
+    let products = [...localProducts];
+    console.log("🟡 Productos locales (fallback inicial):", products);
+
+
+    const response = await fetch(`${API_BASE_URL}/productos`);
+    if (!response.ok) throw new Error("Error al traer productos");
+
+    const data = await response.json();
+    console.log("🟢 Productos desde la API:", data);
+
+    products = data
+
+    return products;
+  } catch (error) {
+    console.error("Error obteniendo productos, usando fallback local:", error);
+    // 4) si falla la API, devolvemos locales
+    return localProducts;
+  }
 };
